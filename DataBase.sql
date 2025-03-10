8ec37eadbfa89998232d3d13603b159fa7c4542b
AryRafael123 <20223tn078@utez.edu.mx>
2025-03-09 23:01:34 -0600

    nueva tabla Opinones en la base de datos

--- DataBase.sql
+++ DataBase.sql
@@ -1,13 +1,128 @@
-SELECT * FROM Usuarios;
-SELECT * FROM Libros;
+CREATE DATABASE libreria;
 
-Drop database libreria;
+USE Libreria;
 
--- usuario administrador
-insert into Usuarios (tipo_usuario, nombre, apellido, nombre_usuario, direccion, teléfono, correo, contraseña)
-values (True, "Ary Rafael", "Sánchez Hernández", "Ary123", "Morelos", "7775955444", "20223tn078@utez.edu.mx", "ary12345") ;
+CREATE TABLE Usuarios (
+			id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+			tipo_usuario BOOLEAN,
+			nombre VARCHAR(50),
+			apellido VARCHAR(50),
+			nombre_usuario VARCHAR(50),
+			direccion VARCHAR(150),
+			teléfono VARCHAR(50),
+			correo VARCHAR(50) UNIQUE,
+			contraseña VARCHAR(50),
+			libros_comprados INT
 
--- usuario normal 
-insert into Usuarios (tipo_usuario, nombre, apellido, nombre_usuario, direccion, teléfono, correo, contraseña)
-values (False, "Jose Emiliano", "Zuñiga Hernández", "Emi123", "Morelos", "777123456", "20223tn117@utez.edu.mx", "emi12345");
+);
+ 
+CREATE TABLE Compras(
+			id_compra INT AUTO_INCREMENT PRIMARY KEY,
+			nombre VARCHAR(50),
+			total DOUBLE
+);
+
+CREATE TABLE Costos(
+			id_precio INT AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE Libros(
+			id_libro INT AUTO_INCREMENT PRIMARY KEY,
+			nombre_libro VARCHAR(50),
+			autor VARCHAR(50),
+			editorial VARCHAR(50),
+			stock INT,
+            imagen VARCHAR(50),
+            descripcion VARCHAR(50)
+);
+
+CREATE TABLE Estado_libros(
+			id_estado INT AUTO_INCREMENT PRIMARY KEY,
+			disponible BOOLEAN,
+			no_disponible BOOLEAN
+);
+
+CREATE TABLE Opiniones(
+			id_opinion INT AUTO_INCREMENT PRIMARY KEY,
+            estrellas INT
+);
+
+SHOW TABLES;
+
+-- Se crean las columnas que tendran las llaves foraneas y se vinculan las llaves foraneas 
+
+ALTER TABLE Compras
+ADD COLUMN id_usuario INT;
+
+ALTER TABLE Compras
+ADD CONSTRAINT fk_Compras_usuario FOREIGN KEY (id_usuario)
+REFERENCES Usuarios (id_usuario);
+
+ALTER TABLE Compras
+ADD COLUMN id_libro INT;
+
+ALTER TABLE Compras
+ADD CONSTRAINT fk_Compras_Libro FOREIGN KEY (id_libro)
+REFERENCES Libros (id_libro);
+
+ALTER TABLE Costos
+ADD COLUMN id_libro INT;
+
+ALTER TABLE Costos
+ADD CONSTRAINT fk_Costos_Libro FOREIGN KEY (id_libro)
+REFERENCES Libros (id_libro)
+ON DELETE CASCADE;
+
+ALTER TABLE Libros
+ADD COLUMN id_precio INT;
+
+ALTER TABLE Libros
+ADD CONSTRAINT fk_Libros_Precio FOREIGN KEY (id_precio)
+REFERENCES Costos (id_precio)
+ON DELETE CASCADE;
+
+ALTER TABLE Estado_libros
+ADD COLUMN id_libro INT;
+
+ALTER TABLE Estado_libros
+ADD CONSTRAINT fk_Estadolibros_Libro FOREIGN KEY (id_libro)
+REFERENCES Libros (id_libro);
+
+-- new lines ------------------
+ALTER TABLE Opiniones 
+ADD COLUMN id_libro INT;
+
+ALTER TABLE Opiniones 
+ADD COLUMN id_usuario INT;
+
+ALTER TABLE Opiniones
+ADD CONSTRAINT fk_Opiniones_Libro FOREIGN KEY (id_libro)
+REFERENCES Libros (id_libro);
+
+ALTER TABLE Opiniones
+ADD CONSTRAINT fk_Opiniones_Usuario FOREIGN KEY (id_usuario)
+REFERENCES Usuarios (id_usuario);
+
+ALTER TABLE Libros
+ADD COLUMN id_opinion INT;
+
+ALTER TABLE Libros
+ADD CONSTRAINT fk_Libros_Opinion FOREIGN KEY (id_opinion)
+REFERENCES Opiniones (id_opinion);
+
+ALTER TABLE Usuarios 
+ADD COLUMN id_opinion INT;
+
+ALTER TABLE Usuarios
+ADD CONSTRAINT fk_Usuarios_Opinion FOREIGN KEY (id_opinion)
+REFERENCES Opiniones (id_opinion);
+
+
+-- end new lines ------------------
+
+
+
+
+ALTER TABLE Costos
+ADD COLUMN precio DOUBLE;
 
